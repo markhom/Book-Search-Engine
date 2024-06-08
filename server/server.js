e07db90e68b6a5b80379de80ef5acb743e856cc2
Mark Homchik <markhomchik4@gmail.com>
2024-06-07 19:53:38 -0500

    adding apollo to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +17,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
+
+// Apply Apollo Server middleware to the Express application
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on http://localhost:${PORT}`);
+      console.log(`🚀 GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+startServer();
