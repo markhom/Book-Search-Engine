84f00482536e6101dad14505f1df87bad3e333db
Mark Homchik <markhomchik4@gmail.com>
2024-06-07 21:43:38 -0500

    fixing mutations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import App from './App.jsx';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-
-// Create HTTP link to the GraphQL server
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// Set up authentication link to include token in headers
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// Initialize Apollo Client
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import App from './App.jsx'
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
 
 const router = createBrowserRouter([
   {
@@ -39,17 +15,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <SearchBooks />
-      },
-      {
+      }, {
         path: '/saved',
         element: <SavedBooks />
       }
     ]
   }
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloProvider client={client}>
-    <RouterProvider router={router} />
-  </ApolloProvider>
-);
+  <RouterProvider router={router} />
+)
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,86 +1,67 @@
-import gql from "graphql-tag";
+//import gql from the @apollo/client library to allow us to parse mutations and queries as template literals
+import { gql } from '@apollo/client';
 
-// mutation for logged in user
+
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
+mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
-        username
         email
-        bookCount
-        savedBooks {
-          bookId
-          title
-          description
-          authors
-          link
-          image
-        }
+        username
       }
     }
   }
 `;
 
-// mutation to add user
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        _id
-        username
         email
-        bookCount
-        savedBooks {
-          bookId
-          title
-          description
-          authors
-          image
-          link
-        }
+        username
+        _id
       }
     }
-  }
+  }  
 `;
 
-// mutation to save books
 export const SAVE_BOOK = gql`
-  mutation SaveBook($book: BookInput) {
-    saveBook(book: $book) {
-      _id
-      username
+mutation saveBook($content: BookContent!) {
+    saveBook(content: $content) {
+      bookCount
       email
       savedBooks {
-        bookId
         authors
-        image
+        bookId
         description
-        title
+        image
         link
+        title
       }
+      username
+      _id
     }
   }
 `;
 
-// mutation to remove books
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
+mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
       _id
-      username
-      email
       bookCount
+      email
       savedBooks {
-        bookId
         authors
-        image
+        bookId
         description
-        title
+        image
         link
+        title
       }
+      username
     }
   }
 `;
